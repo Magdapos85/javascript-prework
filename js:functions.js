fb7ab268039d65f7364b0692040f3cc37b4c0cdf
Magdalena Pospiech <madzia.p3@wp.pl>
2021-02-21 13:02:55 +0100

    new change

--- js:functions.js
+++ js:functions.js
@@ -20,3 +20,33 @@ function printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerMove == 2) {
+	printMessage('papier');
+
+else if (playerMove == 3) {
+printMessage('nożyce');
+
+printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove);
+if(playerMove == 2){
+ printMessage('Ty wygrywasz');
+} else {
+ printMessage('Ty przegrywasz');
+}
+if(playerMove == 3){
+ printMessage('Ty wygrywasz');
+} else {
+ printMessage('Ty przegrywasz');
+}
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
